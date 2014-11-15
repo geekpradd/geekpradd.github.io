15403663656342d662b98ba8df59d13884abbc5b
geekpradd <pradd@outlook.com>
2014-11-15 22:04:14 +0530

    RSS Link fixed

--- public/javascript/ajax-urls.js
+++ public/javascript/ajax-urls.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function($) {
 
     var siteUrl = 'http://'+(document.location.hostname||document.location.host);
-
+    $('.rss').click(function(){
+        window.location=siteUrl+$(this).attr("href");
+    })
     $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
         e.preventDefault();
         $('.loading').css("display","block");
