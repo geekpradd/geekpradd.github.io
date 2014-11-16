a9d7b9107c1a63dce6a0f7ee8bf9681d797b41c0
geekpradd <pradd@outlook.com>
2014-11-16 12:01:29 +0530

    Aasfsf

--- public/javascript/ajax-urls.js
+++ public/javascript/ajax-urls.js
@@ -25,13 +25,21 @@ jQuery(document).ready(function($) {
             document.title = $title;
             $('.content').html($(data).find('.content'));
             $("html, body").animate({ scrollTop: 0 }, "normal");
-            DISQUS.reset({
-  reload: true,
-  config: function () {
-    this.page.identifier = "newidentifier";
-    this.page.url = State.url;
-  }
-});
+            $('#disqus_thread').remove();
+            $('#disq').remove();
+            $('#comment').append("<div id='disqus_thread'></div>
+    <script type='text/javascript' id='disq'>
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'geekpradd'; // required: replace example with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href='http://disqus.com/?ref_noscript'>comments powered by Disqus.</a></noscript>");
             console.log(State.url);
             _gaq.push(['_trackPageview', State.url]);
         });
